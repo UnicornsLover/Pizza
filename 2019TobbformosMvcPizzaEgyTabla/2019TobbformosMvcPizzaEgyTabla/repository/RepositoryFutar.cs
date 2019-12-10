8f53ec71daf5f4a416f9c459bf2bf6402aa7c6f5
István Bálint <istvann.balint@gmail.com>
2019-12-10 14:09:07 +0100

    Repository elkezdve

---
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/repository/RepositoryFutar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbbformosPizzaAlkalmazasEgyTabla.repository
+{
+    partial class RepositoryFutar
+    {
+        List<Futar> futarok;
+
+        public List<Futar> getFutarok()
+        {
+            return futarok;
+        }
+
+        public void setFutarok(List<Futar> futarok)
+        {
+            this.futarok = futarok;
+        }
+
+    }
+}
