e37d02a6ef897fe415ac6a36189b7f48397260f4
István Bálint <istvann.balint@gmail.com>
2019-12-10 12:47:30 +0100

    Jav PizzaFutarkft.cs

--- 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/FormPizzaFutarKft.cs
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/FormPizzaFutarKft.cs
@@ -13,8 +13,6 @@ namespace _2019TobbformosMvcPizzaEgyTabla
 {
     public partial class FormPizzaFutarKft : Form
     {
-
-
         public FormPizzaFutarKft()
         {
             InitializeComponent();
@@ -29,12 +27,12 @@ namespace _2019TobbformosMvcPizzaEgyTabla
 
         private void megrendelőToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            tabControlPizzaFutarKFT.SelectTab("tabPageMegrendelok");
+            tabControlPizzaFutarKFT.SelectTab("tabPageFutarok");
         }
 
         private void futárToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            tabControlPizzaFutarKFT.SelectTab("tabPageFutarok");
+            tabControlPizzaFutarKFT.SelectTab("tabPageMegrendelok");
         }
 
         private void pizzaToolStripMenuItem_Click(object sender, EventArgs e)
@@ -42,44 +40,9 @@ namespace _2019TobbformosMvcPizzaEgyTabla
             tabControlPizzaFutarKFT.SelectTab("tabPagePizzak");
         }
 
-        private void buttonMegsem_Click(object sender, EventArgs e)
+        private void FormPizzaFutarKft_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void buttonModositPizza_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonTorolPizza_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonUjMentes_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxPizzaAr_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxPizzaNev_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonUjPizza_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonBetoltesPizzak_Click(object sender, EventArgs e)
-        {
-
+            beallitGombokatIndulaskor();
         }
     }
 }
