966b7d940d33d34997527f30d8bad6576de14f4c
István Bálint <istvann.balint@gmail.com>
2019-12-10 14:25:59 +0100

    Modell elkezdve

--- 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace TobbbformosPizzaAlkalmazasEgyTabla
 {
-    class Futar
+    partial class Futar
     {
+        private int id;
+        private string name;
+        private string tel;
+
+        public bool isValidName(string name)
+        {
+            if (name == string.Empty)
+            {
+                return false;
+            }
+            if (!char.IsUpper(name.ElementAt(0)))
+            {
+                return false;
+            }
+            for (int i = 1; i < name.Length; i++)
+            {
+                if ((!char.IsLetter(name.ElementAt(i))) && (!char.IsWhiteSpace(name.ElementAt(i))))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Futar(int id, string name, string tel)
+        {
+            this.id = id;
+            if (!isValidName(name))
+                throw new ModelFutarNotValidNameExeption("A futár neve nem megfelelő!");
+            if (!isValidTel(tel))
+                throw new ModelFutarNotValidNameExeption("A telefonszám nem megfelelő!");
+            this.name = name;
+            this.tel = tel;
+        }
+
     }
 }
