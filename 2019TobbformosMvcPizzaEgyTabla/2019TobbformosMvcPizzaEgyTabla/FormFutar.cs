b4f787cd167a7240ea1dd45ac23d9691f02561b1
István Bálint <istvann.balint@gmail.com>
2019-12-11 10:12:01 +0100

    FormFutar

---
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/FormFutar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using TobbbformosPizzaAlkalmazasEgyTabla.Repository;
+using TobbbformosPizzaAlkalmazasEgyTabla.Model;
+using System.Diagnostics;
+
+namespace _2019TobbformosMvcPizzaEgyTabla
+{
+    public partial class FormPizzaFutarKft : Form
+    {
+        
+
+    }
+}
